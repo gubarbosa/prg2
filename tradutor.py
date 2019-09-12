c8d4afbd9d9547b1e449b87fd16cb9ab917d73ab
gubarbosa <gugabarbosa.bsi@gmail.com>
2019-09-12 17:42:13 -0300

    tradutor finalizado

--- tradutor.py
+++ tradutor.py
@@ -1,12 +1,17 @@
 dicionario = {"batatafrita": "frenchfries", "hambúrguer": "hamburguer", "cocacola": "coke",
               "eu": "I", "quero": "want", "e": "and", "com": "with", "carne": "meat", "queijo": "cheese", "para": "for", "hoje": "today", "comer": "eat"}
 
+frase = 'eu quero comer hambúrguer com queijo e carne para hoje'
+x = []
 
-def traduz_para_ingles(texto):
-    for item in dicionario:
-        for palavra in texto:
-            if item.lower() == palavra.lower():
-                novo_texto = texto.replace(palavra, dicionario[item])
-            else:
-                pass
-    return novo_texto
+def traduz_para_ingles(frase):
+    frase_arr = frase.split()
+    for palavra in frase_arr:
+        for i in dicionario.keys():
+            if palavra.lower() == i.lower():
+                x.append(dicionario[i])
+
+    return " ".join(x)
+
+traducao = traduz_para_ingles(frase)
+print(traducao)
